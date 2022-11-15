5a432d31a983726c9f9db59438605a1f4a7ca4b6
Josh <zylo.codes@gmail.com>
2022-11-14 21:17:43 -0600

    Search and Add

--- index.js
+++ index.js
@@ -8,6 +8,58 @@ const connection = mysql.createConnection({
   database: 'sakila'
 })
 
+const searchActors = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Last name: ',
+    }
+  ])
+  
+  try {
+    const [results] = await connection.promise().query(
+      'SELECT * FROM actor WHERE last_name = ?',
+      [answer.last_name]
+    )
+  
+    console.table(results)
+  
+    menuPrompt()
+  } catch(err) {
+    throw new Error(err)
+  }
+}
+
+const addActor = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'What is their first name?'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is their last name?'
+    }
+  ])
+
+  try {
+    const [results] = await connection.promise().query(
+      `INSERT INTO actor (first_name, last_name)
+       VALUES (?, ?)`,
+      [answers.first_name, answers.last_name]
+    )
+  
+    console.log('Actor added!')
+    
+    menuPrompt()
+  } catch(err) {
+    throw new Error(err)
+  }
+}
+
 const menuPrompt = async () => {
   const answers = await inquirer.prompt([
     {
